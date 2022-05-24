641c811d485069c52512f4980b6fbf02784d9479
kloud-linux-xps13 <dgudim@gmail.com>
2022-05-24 15:19:25 +0300

    random image command

---
+++ commands/random_img.ts
@@ -0,0 +1,81 @@
+import { ICommand } from "wokcommands";
+import fs from "fs";
+let currentDirectory = "/home/kloud/Documents/JS";
+
+let path = require('path');
+let walk = function (dir: string, done: Function) {
+    let results: Array<string> = [];
+    fs.readdir(dir, function (err, list) {
+        if (err) return done(err);
+        let i = 0;
+        (function next() {
+            let file = list[i++];
+            if (!file) return done(null, results);
+            file = path.resolve(dir, file);
+            fs.stat(file, function (err, stat) {
+                if (stat && stat.isDirectory()) {
+                    walk(file, function (err: string, res: string) {
+                        results = results.concat(res);
+                        next();
+                    });
+                } else {
+                    if ((file.endsWith(".jpg") || file.endsWith(".png")) 
+                        && fs.statSync(file).size <= 1024 * 8){
+                        results.push(file);
+                    }
+                    next();
+                }
+            });
+        })();
+    });
+};
+
+export default {
+    category: 'Administration',
+    description: 'Get random image from the directory',
+
+    slash: true,
+    testOnly: true,
+    ownerOnly: true,
+    hidden: true,
+
+    expectedArgs: '<directory path>',
+    expectedArgsTypes: ['STRING'],
+    minArgs: 0,
+    maxArgs: 1,
+
+    callback: ({ interaction, args }) => {
+
+        if (args[0]) {
+            if (fs.existsSync(args[0]) && fs.statSync(args[0]).isDirectory()) {
+                interaction.channel?.send({
+                    content: "Changed image directory to " + args[0]
+                });
+                currentDirectory = args[0].endsWith('/') ? args[0] : (args[0] + "/");
+            } else {
+                interaction.channel?.send({
+                    content: "Invalid image directory, will use previous"
+                });
+            }
+        }
+
+        try {
+            walk(currentDirectory, function (err: string, results: Array<string>) {
+                if(err) throw err;
+                let file = results[Math.floor(Math.random() * results.length)];
+
+                interaction.channel?.send({
+                    files: [{
+                        attachment: file,
+                        name: file.substring(file.lastIndexOf('/') + 1)
+                    }]
+                });
+            });
+
+            return "Here is your image"
+        } catch (err) {
+            return "Unknown error: " + err;
+        }
+
+    }
+} as ICommand

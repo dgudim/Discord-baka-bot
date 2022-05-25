45cdcdbdd9740886d5c0028cea2100c7ce00f2b0
kloud-linux-xps13 <dgudim@gmail.com>
2022-05-25 10:08:58 +0300

    autoresize

--- commands/random_img.ts
+++ commands/random_img.ts
@@ -2,9 +2,12 @@ import { ICommand } from "wokcommands";
 import fs from "fs";
 import { config } from "../index"
 import { changeSavedDirectory } from "../utils";
+import sharp from "sharp";
 
 let indexUpToDate = false;
 let index: Array<string> = [];
+
+const eight_mb = 1024 * 1024 * 8;
 
 let walk = function (dir: string) {
     let results: Array<string> = [];
@@ -15,8 +18,10 @@ let walk = function (dir: string) {
         if (stat && stat.isDirectory()) {
             results = results.concat(walk(file));
         } else {
-            if ((file.endsWith(".jpg") || file.endsWith(".jpeg") || file.endsWith(".png")) 
-            && stat && stat.size <= 1024 * 1024 * 8) {
+            if (file.endsWith(".jpg")
+                || file.endsWith(".jpeg")
+                || file.endsWith(".png")
+                || file.endsWith(".webp")) {
                 results.push(file);
             }
         }
@@ -56,13 +61,43 @@ export default {
 
             let file = index[Math.floor(Math.random() * index.length)];
 
-            channel?.send({
-                files: [{
-                    attachment: file,
-                    name: file.substring(file.lastIndexOf('/') + 1)
-                }]
-            });
-
+            if (fs.statSync(file).size > eight_mb) {
+                channel?.send({
+                    content: `image too large, compressing, wait...`
+                });
+                sharp(file)
+                    .resize({ width: 1920 })
+                    .webp({
+                        quality: 80
+                    })
+                    .toBuffer((err, data, info) => {
+                        if (err) {
+                            channel?.send({
+                                content: 'error resizing'
+                            });
+                        } else {
+                            if (info.size > eight_mb) {
+                                channel?.send({
+                                    content: 'image still too large, bruh'
+                                });
+                            } else {
+                                channel?.send({
+                                    files: [{
+                                        attachment: data,
+                                        name: file.substring(file.lastIndexOf('/') + 1)
+                                    }]
+                                });
+                            }
+                        }
+                    });
+            } else {
+                channel?.send({
+                    files: [{
+                        attachment: file,
+                        name: file.substring(file.lastIndexOf('/') + 1)
+                    }]
+                });
+            }
             return "Here is your image"
         } catch (err) {
             return "Unknown error: " + err;

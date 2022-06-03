6443daebcaa7a7bd0935ef8e198cd2f98476428e
dgudim <dgudim@gmail.com>
2022-06-03 11:30:35 +0300

    api calls error catching

--- commands/get_sauce.ts
+++ commands/get_sauce.ts
@@ -8,57 +8,60 @@ const booru = new Danbooru()
 const sagiri_client = sagiri("d78bfeac5505ab0a2af7f19d369029d4f6cd5176");
 
 async function findSauce(file: string, channel: TextBasedChannel | null) {
-    const results = await sagiri_client(file);
+    try {
+        const results = await sagiri_client(file);
 
-    let best_post = results.find((value) => { return value.similarity >= 80 && value.site == 'Danbooru' }) || results[0];
+        let best_post = results.find((value) => { return value.similarity >= 80 && value.site == 'Danbooru' }) || results[0];
 
-    const embed = new MessageEmbed();
-    embed.setTitle(`Result from saucenao`);
-    embed.setColor(perc2color(best_post.similarity));
-    embed.setDescription(`similarity: ${best_post.similarity}`);
-    embed.setURL(best_post.url);
-    embed.setImage(best_post.thumbnail);
-    if (best_post.site == 'Danbooru') {
-        const post = await booru.posts(+getFileName(best_post.url))
-        embed.addFields([{
-            name: "Author",
-            value: post.tag_string_artist || '-'
-        },
-        {
-            name: "Character",
-            value: post.tag_string_character || '-'
-        },
-        {
-            name: "Tags",
-            value: post.tag_string_general || '-'
-        },
-        {
-            name: "Copyright",
-            value: post.tag_string_copyright || '-'
-        }]);
-        if (!isUrl(file)) {
-            setLastTags(new tagContainer(
-                post.tag_string_character,
-                post.tag_string_artist,
-                post.tag_string_general,
-                post.tag_string_copyright,
-                file));
+        const embed = new MessageEmbed();
+        embed.setTitle(`Result from saucenao`);
+        embed.setColor(perc2color(best_post.similarity));
+        embed.setDescription(`similarity: ${best_post.similarity}`);
+        embed.setURL(best_post.url);
+        embed.setImage(best_post.thumbnail);
+        if (best_post.site == 'Danbooru') {
+            const post = await booru.posts(+getFileName(best_post.url))
+            embed.addFields([{
+                name: "Author",
+                value: post.tag_string_artist || '-'
+            },
+            {
+                name: "Character",
+                value: post.tag_string_character || '-'
+            },
+            {
+                name: "Tags",
+                value: post.tag_string_general || '-'
+            },
+            {
+                name: "Copyright",
+                value: post.tag_string_copyright || '-'
+            }]);
+            if (!isUrl(file)) {
+                setLastTags(new tagContainer(
+                    post.tag_string_character,
+                    post.tag_string_artist,
+                    post.tag_string_general,
+                    post.tag_string_copyright,
+                    file));
+            }
+        } else {
+            embed.addFields([{
+                name: "Author",
+                value: best_post.authorName || '-'
+            },
+            {
+                name: "Author url",
+                value: best_post.authorUrl || '-'
+            }]);
         }
-    } else {
-        embed.addFields([{
-            name: "Author",
-            value: best_post.authorName || '-'
-        },
-        {
-            name: "Author url",
-            value: best_post.authorUrl || '-'
-        }]);
+        embed.addField("Site", best_post.site);
+        channel?.send({
+            embeds: [embed]
+        });
+    } catch (err) {
+        sendToChannel(channel, "Error occured while calling the api: " + err);
     }
-    embed.addField("Site", best_post.site);
-    channel?.send({
-        embeds: [embed]
-    });
-
 }
 
 export default {
